d27ed33e9dcd929f480ae73f5886da3b8aa2a68d
Matthew Weier O'Phinney <matthew@zend.com>
2013-07-31 17:16:28 -0500

    Implemented version route prototype/chaining
    - Listens on ModuleEvent::EVENT_MERGE_CONFIG, and modifies configuration
      to add prototype and chains based on configuration in
      zf-versioning.url
    - Will require:
      - Updating ContentType configuration to be in a sub-key
      - Registering the listener on module initialization

---
+++ src/ZF/Versioning/PrototypeRouteListener.php
@@ -0,0 +1,118 @@
+<?php
+/**
+ * @license   http://opensource.org/licenses/BSD-2-Clause BSD-2-Clause
+ */
+
+namespace ZF\Versioning;
+
+use Zend\EventManager\AbstractListenerAggregate;
+use Zend\EventManager\EventManagerInterface;
+use Zend\ModuleManager\Listener\ConfigListener;
+use Zend\ModuleManager\ModuleEvent;
+
+class PrototypeRouteListener extends AbstractListenerAggregate
+{
+    /**
+     * Route prototype to add to router
+     * @var array
+     */
+    protected $versionRoutePrototype = array(
+        'zf_ver_version' => array(
+            'type' => 'segment',
+            'options' => array(
+                'route' => '/v:version',
+                'constraints' => array(
+                    'version' => '\d+',
+                ),
+                'defaults' => array(
+                    'version' => 1,
+                ),
+            ),
+        ),
+    );
+
+    /**
+     * Attach listener to ModuleEvent::EVENT_MERGE_CONFIG
+     * 
+     * @param  EventManagerInterface $events 
+     */
+    public function attach(EventManagerInterface $events)
+    {
+        $this->listeners[] = $events->attach(ModuleEvent::EVENT_MERGE_CONFIG, array($this, 'onMergeConfig'));
+    }
+
+    /**
+     * Listen to ModuleEvent::EVENT_MERGE_CONFIG
+     *
+     * Looks for zf-versioning.url and router configuration; if both present,
+     * injects the route prototype and adds a chain route to each route listed
+     * in the zf-versioning.url array.
+     * 
+     * @param  ModuleEvent $e 
+     */
+    public function onMergeConfig(ModuleEvent $e)
+    {
+        $configListener = $e->getConfigListener();
+        if (!$configListener instanceof ConfigListener) {
+            return;
+        }
+
+        $config = $configListener->getMergedConfig(false);
+
+        // Check for config keys
+        if (!isset($config['zf-versioning'])
+            || !isset($config['router'])
+        ) {
+            return;
+        }
+
+        // Do we need to inject a prototype?
+        if (!isset($config['zf-versioning']['url'])
+            || !is_array($config['zf-versioning']['url'])
+            || empty($config['zf-versioning']['url'])
+        ) {
+            return;
+        }
+
+        // Insert route prototype
+        if (!isset($config['router']['prototypes'])) {
+            $config['router']['prototypes'] = $this->versionRoutePrototype;
+        } else {
+            $config['router']['prototypes'] = array_merge(
+                $config['router']['prototypes'],
+                $this->versionRoutePrototype
+            );
+        }
+
+        // Pre-process route list to strip out duplicates (often a result of
+        // specifying nested routes)
+        $routes   = $config['zf-versioning']['url'];
+        $filtered = array();
+        foreach ($routes as $index => $route) {
+            if (strstr($route, '/')) {
+                $temp  = explode('/', $route, 2);
+                $route = array_shift($temp);
+            }
+            if (in_array($route, $filtered)) {
+                continue;
+            }
+            $filtered[] = $route;
+        }
+        $routes = $filtered;
+
+        // Inject chained routes
+        foreach ($routes as $routeName) {
+            if (!isset($config['router']['routes'][$routeName])) {
+                continue;
+            }
+            if (!isset($config['router']['routes'][$routeName]['chain_routes'])) {
+                $config['router']['routes'][$routeName]['chain_routes'] = array('zf_ver_version');
+            } else {
+                $config['router']['routes'][$routeName]['chain_routes'][] = 'zf_ver_version';
+            }
+        }
+
+        // Reset merged config
+        $configListener->setMergedConfig($config);
+    }
+}
---
+++ test/ZFTest/Versioning/PrototypeRouteListenerTest.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * @license   http://opensource.org/licenses/BSD-2-Clause BSD-2-Clause
+ */
+
+namespace ZFTest\Versioning;
+
+use PHPUnit_Framework_TestCase as TestCase;
+use ReflectionObject;
+use Zend\ModuleManager\Listener\ConfigListener;
+use Zend\ModuleManager\ModuleEvent;
+use ZF\Versioning\PrototypeRouteListener;
+
+class PrototypeRouteListenerTest extends TestCase
+{
+    public function setUp()
+    {
+        $this->config = array('router' => array(
+            'routes' => array(
+                'status' => array(
+                    'type' => 'Segment',
+                    'options' => array(
+                        'route' => '/status[/:id]',
+                        'defaults' => array(
+                            'controller' => 'StatusController',
+                        ),
+                    ),
+                ),
+                'user' => array(
+                    'type' => 'Segment',
+                    'options' => array(
+                        'route' => '/user[/:id]',
+                        'defaults' => array(
+                            'controller' => 'UserController',
+                        ),
+                    ),
+                ),
+            ),
+        ));
+        $this->configListener = new ConfigListener();
+        $this->configListener->setMergedConfig($this->config);
+        $this->event = new ModuleEvent();
+        $this->event->setConfigListener($this->configListener);
+
+    }
+
+    public function routesWithoutPrototype()
+    {
+        return array(
+            'none'   => array(array()),
+            'status' => array(array('status')),
+            'user'   => array(array('user')),
+            'both'   => array(array('status', 'user')),
+        );
+    }
+
+    /**
+     * @dataProvider routesWithoutPrototype
+     */
+    public function testEmptyConfigurationDoesNotInjectPrototypes(array $routes)
+    {
+        $listener = new PrototypeRouteListener();
+        $listener->onMergeConfig($this->event);
+
+        $config = $this->configListener->getMergedConfig(false);
+        $this->assertArrayHasKey('router', $config, var_export($config, 1));
+        $routerConfig = $config['router'];
+        $this->assertArrayNotHasKey('prototypes', $routerConfig);
+
+        $routesConfig = $routerConfig['routes'];
+        foreach ($routes as $routeName) {
+            $this->assertArrayHasKey($routeName, $routesConfig);
+            $routeConfig = $routesConfig[$routeName];
+            $this->assertArrayNotHasKey('chain_routes', $routeConfig);
+        }
+    }
+
+    public function routesForWhichToVerifyPrototype()
+    {
+        return array(
+            'status' => array(array('status')),
+            'user'   => array(array('user')),
+            'both'   => array(array('status', 'user')),
+        );
+    }
+
+    /**
+     * @dataProvider routesForWhichToVerifyPrototype
+     */
+    public function testPrototypeAddedToRoutesProvidedToListener(array $routes)
+    {
+        $this->config['zf-versioning'] = array('url' => $routes);
+        $this->configListener->setMergedConfig($this->config);
+        $listener = new PrototypeRouteListener();
+        $listener->onMergeConfig($this->event);
+
+        $config = $this->configListener->getMergedConfig(false);
+        $this->assertArrayHasKey('router', $config, var_export($config, 1));
+        $routerConfig = $config['router'];
+        $this->assertArrayHasKey('prototypes', $routerConfig, var_export($routerConfig, 1));
+        $this->assertArrayHasKey('zf_ver_version', $routerConfig['prototypes']);
+
+        $routesConfig = $routerConfig['routes'];
+        foreach ($routes as $routeName) {
+            $this->assertArrayHasKey($routeName, $routesConfig);
+            $routeConfig = $routesConfig[$routeName];
+            $this->assertArrayHasKey('chain_routes', $routeConfig);
+            $this->assertEquals(array('zf_ver_version'), $routeConfig['chain_routes']);
+        }
+    }
+}
