5b9ff4a9218f707f52cf01c1a60af0441ebbf36d
Matthew Weier O'Phinney <matthew@zend.com>
2013-07-31 08:20:58 -0500

    Initial structure
    - composer.json
    - Module class
    - test bootstrap and configuration

---
+++ Module.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * @license   http://opensource.org/licenses/BSD-2-Clause BSD-2-Clause
+ */
+
+require __DIR__ . '/src/ZF/Versioning/Module.php';
---
+++ config/module.config.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @license   http://opensource.org/licenses/BSD-2-Clause BSD-2-Clause
+ */
+
+return array(
+    'zf-versioning' => array(
+    ),
+);
