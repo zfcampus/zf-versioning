78dfdf52644b8b13776b1e6448b75a027bee0db5
Matthew Weier O'Phinney <matthew@zend.com>
2013-09-25 08:46:49 -0500

    Add listener to update controller based on version detected

---
+++ src/ZF/Versioning/VersionListener.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * @license   http://opensource.org/licenses/BSD-2-Clause BSD-2-Clause
+ */
+
+namespace ZF\Versioning;
+
+use Zend\EventManager\AbstractListenerAggregate;
+use Zend\EventManager\EventManagerInterface;
+use Zend\Mvc\MvcEvent;
+use Zend\Mvc\Router\RouteMatch;
+
+class VersionListener extends AbstractListenerAggregate
+{
+    /**
+     * @param EventManagerInterface $events
+     */
+    public function attach(EventManagerInterface $events)
+    {
+        $this->listeners[] = $events->attach(MvcEvent::EVENT_ROUTE, array($this, 'onRoute'), -1000);
+    }
+
+    /**
+     * Determine if versioning is in the route matches, and update the controller accordingly
+     *
+     * @param MvcEvent $e
+     */
+    public function onRoute(MvcEvent $e)
+    {
+        $routeMatches = $e->getRouteMatch();
+        if (!$routeMatches instanceof RouteMatch) {
+            return;
+        }
+
+        $version = $this->getVersionFromRouteMatch($routeMatches);
+        if (!$version) {
+            // No version found in matches; done
+            return;
+        }
+
+        $controller = $routeMatches->getParam('controller', false);
+        if (!$controller) {
+            // no controller; we have bigger problems!
+            return;
+        }
+
+        $pattern = '#' . preg_quote('\V') . '(\d+)' . preg_quote('\\') . '#';
+        if (!preg_match($pattern, $controller, $matches)) {
+            // controller does not have a version subnamespace
+            return;
+        }
+
+        $replacement = preg_replace($pattern, '\V' . $version . '\\', $controller);
+        if ($controller === $replacement) {
+            return;
+        }
+        $routeMatches->setParam('controller', $replacement);
+        return $routeMatches;
+    }
+
+    /**
+     * Retrieve the version from the route match.
+     *
+     * The route prototype sets "version", while the Content-Type listener sets
+     * "zf_ver_version"; check both to obtain the version, giving priority to the
+     * route prototype result.
+     * 
+     * @param  RouteMatch $routeMatches 
+     * @return int|false
+     */
+    protected function getVersionFromRouteMatch(RouteMatch $routeMatches)
+    {
+        $version = $routeMatches->getParam('version', false);
+        if ($version) {
+            return $version;
+        }
+        return $routeMatches->getParam('zf_ver_version', false);
+    }
+}
+
